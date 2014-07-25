aa2187c304ce99b02efce3dab65c092496455aac
Rainwater <jqrvz6@mst.edu>
2014-07-25 15:01:11 +0000

    Poke model returns proper json

--- app/models/pokemon.rb
+++ app/models/pokemon.rb
@@ -1,3 +1,7 @@
 class Pokemon < ActiveRecord::Base
   has_and_belongs_to_many :types
+  
+  def as_json
+    super(except: [:id, :created_at, :updated_at], include: [types: {only: :name}])
+  end
 end
