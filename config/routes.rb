7eee268c8f12dd270ca8e77417061c55d093258c
Bollocks <jqrainwater@yahoo.com>
2014-07-25 20:59:26 +0000

    Install bower/angular

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Pokedex::Application.routes.draw do
+  root "poketools#pokedex"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
