395aee70e6758a7aef1ae7de0174669106c81f62
Bollocks <jqrainwater@yahoo.com>
2014-07-25 09:37:23 +0000

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Pokedex::Application.config.secret_key_base = '229416867951502b5874ce451543e03dad5d9b1f652ea6e5d61e862ca80c0ab1126e821943d17a82ecc0ba2fb6f13db98e6ef240291287f1bb692c45c4467941'
