bb031722c467bc82c7af91b2b54b8884d0adebb6
Bollocks <jqrainwater@yahoo.com>
2014-07-26 23:33:22 +0000

    Pokedex api

---
+++ app/controllers/api/v1/pokemons_controller.rb
@@ -0,0 +1,15 @@
+module Api
+  module V1
+    class PokemonsController < ApplicationController
+      respond_to :json
+      
+      def index
+        respond_with Pokemon.all
+      end
+            
+      def show
+        respond_with Pokemon.find_by_national_id(params[:id])
+      end
+    end
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Pokedex::Application.routes.draw do
   root "poketools#pokedex"
+  namespace :api, defaults: {format: 'json'} do
+    namespace :v1 do
+      resources :pokemons, only: [:index, :show]
+    end
+  end
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
