29090f8e5f88ddd71ea5f56571109262c747c139
Rainwater <jqrvz6@mst.edu>
2014-07-26 23:27:10 +0000

    Give as_json an options parameter

--- app/models/pokemon.rb
+++ app/models/pokemon.rb
@@ -1,7 +1,7 @@
 class Pokemon < ActiveRecord::Base
   has_and_belongs_to_many :types
   
-  def as_json
+  def as_json(options = {})
     super(except: [:id, :created_at, :updated_at], include: [types: {only: :name}])
   end
 end
