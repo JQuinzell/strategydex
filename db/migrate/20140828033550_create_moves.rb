458836203c6c62b8e78332dd9badc496e3c25749
Rainwater <jqrvz6@mst.edu>
2014-08-30 05:32:49 +0000

    Giving moves types

--- db/migrate/20140828033550_create_moves.rb
+++ db/migrate/20140828033550_create_moves.rb
@@ -2,6 +2,7 @@ class CreateMoves < ActiveRecord::Migration
   def change
     create_table :moves do |t|
       t.string :name
+      t.string :move_type
       t.string :category
       t.integer :power
 
